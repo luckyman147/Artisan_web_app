0fe7abf0813bd61283ccc1ada2e97c8ac66f8f14
unknown <azzsiri54@gmail.com>
2024-09-16 09:34:49 +0100

    ajouter champs produit

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -10,7 +10,10 @@ const ProductSchema = new mongoose.Schema({
 
   artisan: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   createdAt: { type: Date, default: Date.now },
-  photos: [{ type: String }] 
+  photos: [{ type: String }] ,
+  promo: { type: Boolean, default: false }, // Indicates if the product is on promotion
+  discountPercentage: { type: Number, min: 0, max: 100 } // Discount percentage (0 to 100)
+
 });
 const Product = mongoose.model('Product', ProductSchema);
  
